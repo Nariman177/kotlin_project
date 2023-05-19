6088e614bd82eda23200b5ca84fa352cff99aa90
Nariman <111420981+Nariman177@users.noreply.github.com>
2023-05-19 11:29:47 +0600

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -35,12 +35,96 @@
 //
 //
 //}
-fun main() {
 
-    displayUser("Tom", 23)
-    displayUser("Alice", 19)
-    displayUser("Kate", 25)
+// fun main() {
+
+//     displayUser("Tom", 23)
+//     displayUser("Alice", 19)
+//     displayUser("Kate", 25)
+// }
+// fun displayUser(name: String, age: Int){
+//     println("Name: $name   Age: $age")
+// }
+
+// fun main() {
+//     val tom: Person = Person()
+//     val bob: Person = Person()
+//     val alice: Person = Person()
+// } 
+// class Person
+/*Если свойство определено с помощью val, то значение такого свойства можно установить только один раз, 
+то есть оно immutable. Если свойство определено с помощью var, 
+то значение этого свойства можно многократно изменять.*/
+
+// fun main() {
+//     val bob: Person = Person() // создаем объект
+//     println(bob.name)       // Undefined
+//     println(bob.age)        // 18
+
+//     bob.name = "Berik"
+//     bob.age = 25
+
+//     println(bob.name)       // Bob
+//     println(bob.age)
+// }
+// class Person{
+//     var name: String = "Serik"
+//     var age: Int = 18
+// }
+
+//Функции класса
+
+/*fun main() {
+    val tom = Person()
+    tom.name = "Kambar"
+    tom.age = 37
+
+    tom.sayHello()
+    tom.go("смолға")
+    println(tom.personToString())
 }
-fun displayUser(name: String, age: Int){
-    println("Name: $name   Age: $age")
+class Person{
+    var name: String = "Берік"
+    var age: Int = 18
+
+    fun sayHello(){
+        println("Салем менің атым $name")
+    }
+
+    fun go(location: String){
+        println("$name $location,ға бара жатыр")
+    }
+
+    fun personToString() : String{
+        return "аты: $name  жасы: $age"
+    }
+}*/
+//constructor
+fun main() {
+    val tom = Person("Tom")
+    val bob = Person("Bob", 41)
+    val sam = Person("Sam", 32, "JetBtains")
+
+    println("Name: ${tom.name}  Age: ${tom.age}  Company: ${tom.company}")
+    println("Name: ${bob.name}  Age: ${bob.age}  Company: ${bob.company}")
+    println("Name: ${sam.name}  Age: ${sam.age}  Company: ${sam.company}")
 }
+class Person(_name: String){
+    val name = _name
+    var age: Int = 0
+    var company: String = "Undefined"
+
+    constructor(_name: String, _age: Int) : this(_name){
+        age = _age
+    }
+
+    constructor(_name: String, _age: Int, _comp: String) : this(_name, _age){
+        company = _comp
+    }
+}
+
+
+
+
+
+
