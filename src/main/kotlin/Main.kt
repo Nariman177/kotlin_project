8aae8eda0640966b6cc1676e72b30f9ae2411fe7
Nariman <111420981+Nariman177@users.noreply.github.com>
2023-05-22 11:45:36 +0600

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -154,6 +154,64 @@ class Person(val name: String){
         get()=  _age
 }
 
+Интерфейсы
+interface Movable{
+    var speed: Int  // объявление свойства
+    fun move()      // определение функции без реализации
+    fun stop(){     // определение функции с реализацией по умолчанию
+        println("Остановка")
+    }
+}
+class Car : Movable{
+
+    override var speed = 60
+    override fun move(){
+        println("Машина едет со скоростью $speed км/ч")
+    }
+}
+class Aircraft : Movable{
+
+    override var speed = 600
+    override fun move(){
+        println("Самолет летит со скоростью $speed км/ч")
+    }
+    override fun stop(){
+        println("Приземление")
+    }
+}
+fun main() {
+    val m1: Movable = Car()
+    val m2: Movable = Aircraft()
+    // val m3: Movable = Movable() напрямую объект интерфейса создать нельзя
+    m1.move()
+    m1.stop()
+    m2.move()
+    m2.stop()
+}
+
+Реализация свойств
+interface Info{
+    val model: String
+        get() = "Undefined"
+    val number: String
+}
+class Car(override val model: String, override var number: String) : Movable, Info{
+ 
+    override var speed = 60
+    override fun move(){
+        println("Машина едет со скоростью $speed км/ч")
+    }
+}
+fun main() {
+ 
+    val tesla: Car = Car("Tesla", "2345SDG")
+    println(tesla.model)
+    println(tesla.number)
+ 
+    tesla.move()
+    tesla.stop()
+}
+
 
 
 
